6ec90ef9e8b90b8c4eb147d0fe9cbcf897a38dd3
cinthiadepaula <cinthiade.paula@outlook.com>
2024-08-27 14:41:17 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,59 +1,72 @@
+let img, imgX = 0, imgY = 0, dragging = false;
+
 document.getElementById('imageInput').addEventListener('change', function() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const file = this.files[0];
     const reader = new FileReader();
     const watermark = new Image();
-    
-    watermark.src = 'assets/marca_dagua3.png';  // Caminho para a sua marca d'água fixa
 
-    watermark.onload = function() {
-        console.log('Marca d\'água carregada');
-    };
-
-    watermark.onerror = function() {
-        console.error('Erro ao carregar a marca d\'água');
-    };
+    watermark.src = 'assets/marca_dagua3.png';
 
     reader.onload = function(e) {
-        const img = new Image();
+        img = new Image();
         img.src = e.target.result;
         
         img.onload = function() {
-            canvas.width = img.width;
-            canvas.height = img.height;
-
-            // Desenha a imagem do usuário no canvas
-            ctx.drawImage(img, 0, 0);
-
-            // Aguarde até a marca d'água carregar completamente
-            watermark.onload = function() {
-                // Proporção da marca d'água em relação à imagem original
-                const watermarkRatio = 1.0;  // Ajuste essa proporção conforme necessário
-                const watermarkWidth = img.width * watermarkRatio;
-                const watermarkHeight = watermark.height * (watermarkWidth / watermark.width);
-                
-                // Posicionamento da marca d'água no centro da imagem
-                const posX = (img.width - watermarkWidth) / 2;
-                const posY = (img.height - watermarkHeight) / 1.0;
-                
-                ctx.globalAlpha = 1.0;  // Ajuste de opacidade da marca d'água (pode variar entre 0 e 1)
-                ctx.drawImage(watermark, posX, posY, watermarkWidth, watermarkHeight);
-                ctx.globalAlpha = 1.0;
-                
-                 // Exibir o botão de download após o upload e renderização
-                 document.getElementById('downloadBtn').style.display = 'inline-flex';
-            };
-
-            // Força o carregamento da marca d'água, caso ainda não tenha carregado
-            if (watermark.complete) {
-                watermark.onload();
-            }
+            imgX = (canvas.width - img.width) / 2;
+            imgY = (canvas.height - img.height) / 2;
+            drawCanvas(ctx, canvas, img, watermark, imgX, imgY);
         };
     };
 
     reader.readAsDataURL(file);
+
+    canvas.onmousedown = startDragging;
+    canvas.onmousemove = dragImage;
+    canvas.onmouseup = stopDragging;
+    canvas.onmouseleave = stopDragging;
 });
+
+function drawCanvas(ctx, canvas, img, watermark, imgX, imgY) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, imgX, imgY);
+    const watermarkWidth = 1080;
+    const watermarkHeight = watermark.height * (watermarkWidth / watermark.width);
+    const posX = 0;
+    const posY = canvas.height - watermarkHeight;
+    ctx.globalAlpha = 1.0;
+    ctx.drawImage(watermark, posX, posY, watermarkWidth, watermarkHeight);
+}
+
+function startDragging(e) {
+    const canvas = document.getElementById('canvas');
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    if (mouseX >= imgX && mouseX <= imgX + img.width &&
+        mouseY >= imgY && mouseY <= imgY + img.height) {
+        dragging = true;
+    }
+}
+
+function dragImage(e) {
+    if (dragging) {
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+        const rect = canvas.getBoundingClientRect();
+        imgX = e.clientX - rect.left - img.width / 2;
+        imgY = e.clientY - rect.top - img.height / 2;
+        const watermark = new Image();
+        watermark.src = 'assets/marca_dagua3.png';
+        drawCanvas(ctx, canvas, img, watermark, imgX, imgY);
+    }
+}
+
+function stopDragging() {
+    dragging = false;
+}
 
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const canvas = document.getElementById('canvas');
